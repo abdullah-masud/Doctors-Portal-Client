886673f8bc1a95a864441f3c374209bb1a32a331
Abdullah Al Masud <masudctg00@gmail.com>
2022-05-12 00:41:14 +0600

    get-started done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from './Banner';
+import GetStarted from './GetStarted';
 import Info from './Info';
+import Services from './Services';
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <Info />
+            <Services />
+            <GetStarted />
         </div>
     );
 };
--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -7,7 +7,7 @@ import phone from '../../assets/icons/phone.svg'
 const Info = () => {
 
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-14'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 container mx-auto px-12'>
             <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardInfo="Lorem Ipsum is simply dummy text of the pri " cardTitle="Opening Hours" img={clock} />
             <InfoCard bgClass="bg-accent" cardInfo="Brooklyn, NY 10036, United States" cardTitle="Visit Our Location " img={marker} />
             <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardInfo="+000 123 456789" cardTitle="Contact Us Now" img={phone} />

8b54e2b83be561d1e558ff8b02930584b224e6cd
Abdullah Al Masud <masudctg00@gmail.com>
2022-05-14 00:11:35 +0600

    calender added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Pages/Shared/Navbar';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Login from './Pages/Login/Login';
+import Appointment from './Pages/Appointment/Appointment';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/appointment" element={<Appointment />} />
         <Route path="about" element={<About />} />
         <Route path="login" element={<Login />} />
       </Routes>
